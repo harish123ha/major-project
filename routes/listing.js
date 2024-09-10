914066e5bced12d52996100af6f588ea21f6f6eb
harish123ha <harishupadhyay999912@gmail.com>
2024-09-10 18:50:25 +0530

    modified url in index.ejs

--- routes/listing.js
+++ routes/listing.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const wrapAsync = require("../utils/wrapAsync.js");
 const isLoggedIn = require("../middleware.js");
 const { isOwner } = require("../middleware.js");
